a3ae1c4b8cca833eb3f61e7c499a6d8f99efb199
Dodi Aditya <dodiaridharma@gmail.com>
2025-08-13 11:19:47 +0800

    chore: setup and add scss config

---
+++ nuxt.config.ts
@@ -0,0 +1,17 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  compatibilityDate: "2025-07-15",
+  devtools: { enabled: true },
+  srcDir: "src/",
+  serverDir: "server/",
+  css: ["~/assets/scss/main.scss"],
+  vite: {
+    css: {
+      preprocessorOptions: {
+        scss: {
+          // additionalData: '@use "/assets/scss/config" as *;',
+        },
+      },
+    },
+  },
+});
